fe97dda50e0e6f96f89793e1ea8ad1789ad17633
Vlad <55617080+forkmalik@users.noreply.github.com>
2021-08-01 16:31:59 +0300

    Work with style

--- biy/src/Components/Header/Header.jsx
+++ biy/src/Components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import headerStyle from "./Header.module.scss";
+import style from "./Header.module.scss";
+import logoImage from "../../img/ittle-logo.png";
 
 const Header = () => {
   return (
-    <header class="header">
-      <h2 class="main-text animated pulse">Book It Yourself</h2>
-      <button class="header__button">Book Now</button>
+    <header className={style.header}>
+      <div className={style.header__container}>
+        <h1 className={style.header__text}>Book It Yourself</h1>
+        <button className={style.header__button}>Book Now</button>
+      </div>
+      <img className={style.header__logo} src={logoImage} alt="" />
     </header>
   );
 };
