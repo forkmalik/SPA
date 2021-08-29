0bce3206886c50dd6839192899036e6338314b3b
Vlad <55617080+forkmalik@users.noreply.github.com>
2021-08-29 22:18:24 +0300

    carousele

--- biy/src/Components/Carousel/Carousel.jsx
+++ biy/src/Components/Carousel/Carousel.jsx
@@ -1,31 +1,25 @@
 import React from "react";
 import style from "./Carousel.module.scss";
-import TouchCarousel from 'react-touch-carousel';
 import Type from "../TypePage/ClassTypes/Type/Type";
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
 
 const classData = [
-    {title: "Lecture class", time: "1:20 h"},
-    {title: "Lecture class", time: "1:20 h"},
-    {title: "Lecture class", time: "1:20 h"},
-    {title: "Lecture class", time: "1:20 h"}
+  { title: "Lecture class", time: "1:20 h" },
+  { title: "Lecture class", time: "1:20 h" },
+  { title: "Lecture class", time: "1:20 h" },
+  { title: "Lecture class", time: "1:20 h" },
 ];
 
-function CarouselContainer (props) {
-    return <Type />;
-};
-
-function renderCard (index, modIndex, cursor) {
-    const item = classData[modIndex];
-};
-
 const Carousel = () => {
-    return(
-    <TouchCarousel 
-        component={CarouselContainer}
-        carCount={classData.length}
-        cardSize={375}
-        renderCard={renderCard}
-    />);
+   return(<OwlCarousel className='owl-theme'>
+    <Type />
+    <Type />
+    <Type />
+    <Type />    
+</OwlCarousel>); 
 };
 
 export default Carousel;
